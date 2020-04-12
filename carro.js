e638d86834f4bb472e34a41276787eed1005abdf
Wagner vicente <Wagner.vicentebr@gmail.com>
2020-04-12 18:11:06 -0300

    carros andando

--- carro.js
+++ carro.js
@@ -1,18 +1,53 @@
 class Carro {
-    constructor(ctx, left, top) {
+    constructor(ctx, cenario, idPista, left, top, maxSpeed) {
         this.ctx = ctx;
+        this.cenario = cenario;
+        this.idPista = idPista;
         this.left = left;
         this.top = top;
         this.CAR_COLORS = ['white', 'red', 'green', 'blue', 'lightblue', 'lightgreen'];
         this.cor = this.getRandom(this.CAR_COLORS);
+        this.contFrames = 0;
+        this.maxFrames = 50;
+        this.width = 40;
+        this.height = 15
+
+        this.speed = 0;
+        this.aceleration = 0.02;
+        this.maxSpeed = maxSpeed + (maxSpeed * Math.random());
     }
 
     atualizar() {
-
+        // if (this.contFrames++ > this.maxFrames) {
+            let dist = this.cenario.distProximoCarro(this, this.idPista);
+            // console.log(dist);
+            this.contFrames = 0;
+            if (dist < 20) {
+                this.muitoPerto = true;
+                this.speed -= this.aceleration * 2;
+                if (this.speed < 0) {
+                    this.speed = 0;
+                }
+                // this.maxSpeed = this.maxSpeed * 0.995;
+            } else {
+                this.muitoPerto = false;
+            }
+        // }
+        if (!this.muitoPerto) {
+            this.speed += this.aceleration;
+        }
+        // this.speed = 0.3;
+        if (this.speed > this.maxSpeed) {
+            this.speed = this.maxSpeed;
+        }
+        this.left += this.speed;
+        if (this.left > LARGURA - 70) {
+            this.cenario.removerCarro(this, this.idPista);
+        }
     }
 
     desenhar() {
-        draw.drawRect(this.left, this.top, 40, 15, 1, this.cor, this.cor);
+        draw.drawRect(this.left, this.top, this.width, this.height, 1, this.cor, this.cor);
     }
 
     getRandom(array) {
--- cenario.js
+++ cenario.js
@@ -2,21 +2,71 @@ class Cenario {
     constructor(ctx) {
         this.ctx = ctx;
         let top = (ALTURA / 2 - 80);
+        this.top = top;
         this.pista = new Pista(ctx, top);
         this.carros = [];
-        this.carros.push(new Carro(ctx, 50, top + 7));
+        this.maxSpeed = 2;
+        this.contFrames = 0;
+        this.maxFramesMinimo = 20;
+        this.maxFramesMaximo = 30;
+        this.maxFrames = this.maxFramesMinimo + Math.floor(Math.random() * this.maxFramesMaximo);
+        this.pistas = [[], [], []];
+    }
+
+    removerCarro(carro, idPista) {
+        let carros = this.pistas[idPista];
+        carros.shift();
+    }
+
+    distProximoCarro(carro, idPista) {
+        let carros = this.pistas[idPista];
+        for (let i = 0; i < carros.length; i++) {
+            if (carros[i] === carro) {
+                if (i == 0) {
+                    return 1000;
+                }
+                const carroAnterior = carros[i - 1];
+                return carroAnterior.left - carro.left - carroAnterior.width;
+            }
+        }
     }
 
     atualizar() {
-        this.carros.forEach(carro => {
-            carro.atualizar();
+        this.contFrames++;
+        if (this.contFrames > this.maxFrames) {
+            this.maxSpeed += 0.1;
+            this.maxFramesMinimo--;
+            this.maxFramesMaximo--;
+            if (this.maxFramesMinimo < 10) {
+                this.maxFramesMinimo = 10
+            }
+            if (this.maxFramesMaximo < 10) {
+                this.maxFramesMaximo = 10
+            }
+            this.contFrames = 0;
+            let idPista = Math.floor(Math.random() * 3);
+            while (idPista == this.lastIdPista) {
+                idPista = Math.floor(Math.random() * 3);
+            }
+            this.lastIdPista = idPista;
+            let distPista = 30;
+            this.pistas[idPista].push(new Carro(this.ctx, this, idPista, 50, this.top + 7 + (distPista * idPista), this.maxSpeed));
+            this.stopAddingCar = true;
+            this.maxFrames = this.maxFramesMinimo + Math.floor(Math.random() * this.maxFramesMaximo);
+        }
+        this.pistas.forEach(carros => {
+            carros.forEach(carro => {
+                carro.atualizar();
+            })
         });
     }
 
     desenhar() {
         this.pista.desenhar();
-        this.carros.forEach(carro => {
-            carro.desenhar();
+        this.pistas.forEach(carros => {
+            carros.forEach(carro => {
+                carro.desenhar();
+            })
         });
     }
 }
